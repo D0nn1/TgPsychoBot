c6f13174c25a337f330c5b01eb087d5dddefbe16
Roman Tynchirov <tynchirov.r@gmail.com>
2024-04-10 18:56:34 +0300

    Initial commit

---
+++ src/main/java/com/example/tgpsychobot/TgPsychoBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.tgpsychobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgPsychoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgPsychoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tgpsychobot/TgPsychoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgpsychobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgPsychoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
