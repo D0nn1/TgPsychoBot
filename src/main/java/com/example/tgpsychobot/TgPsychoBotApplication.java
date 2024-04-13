eb5c39ba8c1b6f18dd56a750d4568eaa65a3dca0
Roman Tynchirov <tynchirov.r@gmail.com>
2024-04-13 16:17:50 +0300

    added menu and buttons

--- src/main/java/com/example/tgpsychobot/TgPsychoBotApplication.java
+++ src/main/java/com/example/tgpsychobot/TgPsychoBotApplication.java
@@ -1,12 +1,15 @@
 package com.example.tgpsychobot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class TgPsychoBotApplication {
 
 	public static void main(String[] args) {
+		log.info("\n ################### \n TgPsychoBot started \n ###################" );
 		SpringApplication.run(TgPsychoBotApplication.class, args);
 	}
 
--- src/main/java/com/example/tgpsychobot/config/BotInitializer.java
+++ src/main/java/com/example/tgpsychobot/config/BotInitializer.java
@@ -2,6 +2,7 @@ package com.example.tgpsychobot.config;
 
 
 import com.example.tgpsychobot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+
+@Slf4j
 @Component
 public class BotInitializer {
 
@@ -24,6 +27,7 @@ public class BotInitializer {
         try {
             telegramBotsApi.registerBot(telegramBot);
         } catch (TelegramApiException e) {
+            log.error("Error occured: " + e.getMessage());
             e.printStackTrace();
         }
     }
